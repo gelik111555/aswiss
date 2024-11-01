bc12d1587b5e189394a5d5a25af16e54fa70042e
da4a <gelik111555@gmail.com>
2024-11-01 14:51:47 +0300

    feat: add validation for GetUsersQuery to ensure correct search parameters
    - Implemented GetUsersQueryValidator to enforce search criteria requirements.
    - Ensures at least one search parameter is provided (UserId, LastName, FirstName, MiddleName, PhoneNumber, or Email).
    - Validates LastName, FirstName, and MiddleName for allowed characters.
    - Validates PhoneNumber to contain only digits.
    - Checks Email format to be valid when provided.

---
+++ src/Application/Users/Queries/GetUsers/GetUsersQueryValidator.cs
@@ -0,0 +1,38 @@
+﻿namespace ASWISS.Application.Users.Queries.GetUsers;
+
+public class GetUsersQueryValidator : AbstractValidator<GetUsersQuery>
+{
+    public GetUsersQueryValidator()
+    {
+        RuleFor(x => x)
+            .Must(x => x.UserId != null || !string.IsNullOrEmpty(x.LastName) ||
+                       !string.IsNullOrEmpty(x.FirstName) || !string.IsNullOrEmpty(x.MiddleName) ||
+                       !string.IsNullOrEmpty(x.PhoneNumber) || !string.IsNullOrEmpty(x.Email))
+            .WithMessage("Необходимо указать хотя бы один параметр для поиска.");
+
+        RuleFor(x => x.LastName)
+            .Matches(@"^[a-zA-Zа-яА-ЯёЁ]*$")
+            .When(x => !string.IsNullOrEmpty(x.LastName))
+            .WithMessage("Фамилия содержит недопустимые символы.");
+
+        RuleFor(x => x.FirstName)
+            .Matches(@"^[a-zA-Zа-яА-ЯёЁ]*$")
+            .When(x => !string.IsNullOrEmpty(x.FirstName))
+            .WithMessage("Имя содержит недопустимые символы.");
+
+        RuleFor(x => x.MiddleName)
+            .Matches(@"^[a-zA-Zа-яА-ЯёЁ]*$")
+            .When(x => !string.IsNullOrEmpty(x.MiddleName))
+            .WithMessage("Отчество содержит недопустимые символы.");
+
+        RuleFor(x => x.PhoneNumber)
+            .Matches(@"^\d*$")
+            .When(x => !string.IsNullOrEmpty(x.PhoneNumber))
+            .WithMessage("Номер телефона содержит недопустимые символы.");
+
+        RuleFor(x => x.Email)
+            .EmailAddress()
+            .When(x => !string.IsNullOrEmpty(x.Email))
+            .WithMessage("Некорректный формат email.");
+    }
+}
