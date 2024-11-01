70f4de205db45fab3137aa949ed9085712578e28
da4a <gelik111555@gmail.com>
2024-11-01 14:58:49 +0300

    feat: create User endpoint with CreateUser and GetUsers methods

--- src/Web/Endpoints/Users.cs
+++ src/Web/Endpoints/Users.cs
@@ -1,4 +1,5 @@
-﻿using ASWISS.Infrastructure.Identity;
+﻿using ASWISS.Application.Users.Commands.CreateUser;
+using ASWISS.Application.Users.Queries.GetUsers;
 
 namespace ASWISS.Web.Endpoints;
 
@@ -7,6 +8,17 @@ public class Users : EndpointGroupBase
     public override void Map(WebApplication app)
     {
         app.MapGroup(this)
-            .MapIdentityApi<ApplicationUser>();
+            .MapPost(CreateUser)
+            .MapGet(GetUsers);
+    }
+
+    public async Task<List<UserDto>> GetUsers(ISender sender, [AsParameters] GetUsersQuery query)
+    {
+        return await sender.Send(query);
+    }
+
+    public Task<string> CreateUser(ISender sender, CreateUserCommand command)
+    {
+        return sender.Send(command);
     }
 }
