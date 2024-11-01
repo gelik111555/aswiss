f350cb46d640b5004048a64e26670702a9ae687e
da4a <gelik111555@gmail.com>
2024-11-01 14:49:19 +0300

    feat: add GetUsersQuery to enable user search by multiple criteria
    - Implemented GetUsersQuery to allow searching users by ID, last name, first name, middle name, phone number, and email.
    - Created GetUsersQueryHandler to handle the query by utilizing the FindUsersAsync method in IIdentityService.

---
+++ src/Application/Users/Queries/GetUsers/GetUsers.cs
@@ -0,0 +1,34 @@
+﻿using ASWISS.Application.Common.Interfaces;
+
+namespace ASWISS.Application.Users.Queries.GetUsers;
+
+public record GetUsersQuery : IRequest<List<UserDto>>
+{
+    public Guid? UserId { get; init; } // Поиск по ID
+    public string? LastName { get; init; } // Поиск по фамилии
+    public string? FirstName { get; init; } // Поиск по имени
+    public string? MiddleName { get; init; } // Поиск по отчеству
+    public string? PhoneNumber { get; init; } // Поиск по телефону
+    public string? Email { get; init; } // Поиск по email
+}
+
+public class GetUsersQueryHandler : IRequestHandler<GetUsersQuery, List<UserDto>>
+{
+    private readonly IIdentityService _identityService;
+
+    public GetUsersQueryHandler(IIdentityService identityService)
+    {
+        _identityService = identityService;
+    }
+
+    public async Task<List<UserDto>> Handle(GetUsersQuery request, CancellationToken cancellationToken)
+    {
+       return await _identityService.FindUsersAsync(
+            request.UserId,
+            request.LastName,
+            request.FirstName,
+            request.MiddleName,
+            request.PhoneNumber,
+            request.Email);
+    }
+}
