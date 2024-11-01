84010ea76381419c0f34095c13a029296e466d32
da4a <gelik111555@gmail.com>
2024-11-01 14:52:28 +0300

    feat: implement CreateUserCommand for user creation with validation and logging
    - Added CreateUserCommand to encapsulate user creation data, including password, personal information, and address.
    - Developed CreateUserCommandHandler to handle user creation logic.
    - Mapped CreateUserCommand to CreateUserOptions for use with IIdentityService.
    - Implemented error handling and logging to capture and report any issues during user creation.
    - Returns newly created user's ID or logs an error with specific validation failure details.

---
+++ src/Application/Users/Commands/CreateUser/CreateUser.cs
@@ -0,0 +1,57 @@
+﻿using System.Text.RegularExpressions;
+using ASWISS.Application.Common.Interfaces;
+using ASWISS.Application.Common.Models;
+using Microsoft.Extensions.Logging;
+
+namespace ASWISS.Application.Users.Commands.CreateUser;
+
+public record CreateUserCommand : IRequest<string>
+{
+    public required string Password { get; init; }
+    public string? LastName { get; init; }
+    public string? FirstName { get; init; }
+    public string? MiddleName { get; init; }
+    public DateTime? DateOfBirth { get; init; }
+    public string? PassportNumber { get; init; }
+    public string? PlaceOfBirth { get; init; }
+    public string? PhoneNumber { get; init; }
+    public string? Email { get; init; }
+    public string? RegistrationAddress { get; init; }
+    public string? ResidentialAddress { get; init; }
+}
+
+public class CreateUserCommandHandler : IRequestHandler<CreateUserCommand, string>
+{
+    private readonly IMapper _mapper;
+    private readonly IIdentityService _identityService;
+    private readonly ILogger<CreateUserCommandHandler> _logger;
+
+    public CreateUserCommandHandler
+        (
+        IMapper mapper,
+        IIdentityService identityService,
+        ILogger<CreateUserCommandHandler> logger
+        )
+    {
+        _mapper = mapper;
+        _identityService = identityService;
+        _logger = logger;
+    }
+
+    public async Task<string> Handle(CreateUserCommand request, CancellationToken cancellationToken)
+    {
+        var options = _mapper.Map<CreateUserOptions>(request);
+
+        var result =  await _identityService.CreateUserAsync(options);
+
+         if(result.Result.Succeeded)
+        {
+            return result.UserId.ToString();
+        }
+        var errorMessages = string.Join("; ", result.Result.Errors.Select(e => e));
+
+        _logger.LogError("Ошибка при создании пользователя: {ErrorMessages}", errorMessages);
+
+        throw new Exception($"Ошибка при создании пользователя: {errorMessages}");
+    }
+}
