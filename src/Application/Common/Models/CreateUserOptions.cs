4ab9a650d70e59ac29d55389ef89c5c6b7d91b78
da4a <gelik111555@gmail.com>
2024-11-01 14:57:15 +0300

    feat: add CreateUserOptions model and mapping configuration

---
+++ src/Application/Common/Models/CreateUserOptions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ASWISS.Application.Users.Commands.CreateUser;
+
+namespace ASWISS.Application.Common.Models;
+public class CreateUserOptions
+{
+    public required string Password { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? MiddleName { get; set; }
+    public DateTime? DateOfBirth { get; set; }
+    public string? PassportNumber { get; set; }
+    public string? PlaceOfBirth { get; set; }
+    public string? PhoneNumber { get; set; }
+    public string? Email { get; set; }
+    public string? RegistrationAddress { get; set; }
+    public string? ResidentialAddress { get; set; }
+
+    private class Mapping : Profile
+    {
+        public Mapping()
+        {
+            CreateMap<CreateUserCommand, CreateUserOptions>();
+        }
+    }
+}
